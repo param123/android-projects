3353494b9376fe33978899f77e9f1f8cc6dab428
param123 <singh.prabhat@gmail.com>
2011-02-07 19:35:06 +0530

    included menu and dialogbox with options of difficulties

--- src/org/game/Sudoku.java
+++ src/org/game/Sudoku.java
@@ -1,12 +1,20 @@
 package org.game;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 public class Sudoku extends Activity implements OnClickListener{
+	
+	private static final String TAG = "Sudoku";
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,14 +29,54 @@ public class Sudoku extends Activity implements OnClickListener{
         View exit_button=findViewById(R.id.exit_button);
         exit_button.setOnClickListener(this);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+    	return true;
+    }
     
+    @Override
+    public boolean onOptionsItemSelected(android.view.MenuItem item) {
+		
+    	switch(item.getItemId()){
+    	case R.id.settings:
+    		startActivity(new Intent(this, Prefs.class));
+    		return true;
+    	
+    	}
+    	
+    	return false;
+    };
+    
+    @Override
 	public void onClick(View v) {
 		switch(v.getId()){
 		case R.id.about_button:
 			 Intent i = new Intent(this,About.class);
 			 startActivity(i);
 			 break;
-		
+		case R.id.new_button:
+			 openNewGameDialog();
+			 break;
 		}
 	}
+    
+    private void openNewGameDialog(){
+    	new AlertDialog.Builder(this)
+    	.setTitle(R.string.new_game_title)
+    	.setItems(R.array.difficulty,
+    			new DialogInterface.OnClickListener() {
+    			public void onClick(DialogInterface dialoginterface, int i){
+    			   startGame(i);
+    			  }
+    			})
+    			.show();
+    }
+    
+    private void startGame(int i){
+    	Log.d(TAG, "clicked on" +i);
+    	}
+    	
 }
